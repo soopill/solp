3130868dfb5158e1889644977a1723020b9ae9ad
soo pill <soopill0405@naver.com>
2024-11-05 19:36:30 +0900

    집중력떨어짐

--- ppp/01-starting-project/src/App.jsx
+++ ppp/01-starting-project/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000, //초기투자
+    annualInvestment: 1200, //연간투자
+    expectedReturn: 6, //예상수익률
+    duration: 10, //지속시간
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return { ...prevUserInput, [inputIdentifier]: newValue };
+    });
+  }
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} />;
+    </>
+  );
 }
 
 export default App;
