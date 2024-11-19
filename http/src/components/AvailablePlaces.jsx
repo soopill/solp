4ead628f1331a57765dc707ba96332be47eeaac8
soo pill <soopill0405@naver.com>
2024-11-19 21:49:36 +0900

    fetch 따로 뺐음

--- http/src/components/AvailablePlaces.jsx
+++ http/src/components/AvailablePlaces.jsx
@@ -1,22 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
-import { useEffect } from "react";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
   useEffect(() => {
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({ message: error.message || "장소 못불러옴 다시 ㄱ" });
+        setIsFetching(false);
+      }
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data"
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- sideE/src/App.jsx
+++ sideE/src/App.jsx
@@ -93,7 +93,7 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
           fallbackText="Sorting places by distance"
           onSelectPlace={handleSelectPlace}
         />
