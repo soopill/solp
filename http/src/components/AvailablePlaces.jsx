5836a013f8b3f72587f884c5473ea2029c803755
soo pill <soopill0405@naver.com>
2024-11-19 18:50:46 +0900

    http 나름재밋음

---
+++ http/src/components/AvailablePlaces.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Places from "./Places.jsx";
+import { useEffect } from "react";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+    fetchPlaces();
+  }, []);
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
