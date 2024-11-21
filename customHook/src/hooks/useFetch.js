ce188115dc540137b53619bfb29176016e91e056
soo pill <soopill0405@naver.com>
2024-11-21 17:27:24 +0900

    커스텀훅 개념알고 다시

---
+++ customHook/src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function useFetch(fetchFn) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(places);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch user places." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+}
